bc6a1f12745fbf17b85412aba0aca8c447d1fb2a
Priyanshu Singh <priyanshusighrajput007@gmail.com>
2024-12-17 16:38:17 +0530

    Literal and data tupes

--- Literals.java
+++ Literals.java
@@ -4,10 +4,12 @@ public class Literals{
         int age2 = 20;
         float age3 = 3.5f;
         char ch = 'T';
+        String string = "FOUR";
         System.out.println(age);
         System.out.println(age2);
         System.out.println(age3);
         System.out.println(ch);
+        System.out.println(string);
 
 
     }
