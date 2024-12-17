d33854e2cf4fee2b17feed873a3d05caa719d28d
Priyanshu Singh <priyanshusighrajput007@gmail.com>
2024-12-17 16:13:45 +0530

    Sum of numbers programme

---
+++ Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+    public static void main(String[]args){
+        System.out.print("The Sum of the give numbers is :");
+        int num1 = 5;
+        int num2 = 6;
+        int num3 = 10;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
