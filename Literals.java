5abf3e0ee7b33029b05d00157946e6160e594f7f
Priyanshu Singh <priyanshusighrajput007@gmail.com>
2024-12-17 16:35:32 +0530

    Literals programme

---
+++ Literals.java
@@ -0,0 +1,14 @@
+public class Literals{
+    public static void main(String[] args) {
+        byte age =  18;
+        int age2 = 20;
+        float age3 = 3.5f;
+        char ch = 'T';
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(age3);
+        System.out.println(ch);
+
+
+    }
+}
