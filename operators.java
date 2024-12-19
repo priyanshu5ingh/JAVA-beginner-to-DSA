14cf4aba55e96c18ada0b3208093b7c9165f4440
Priyanshu Singh <priyanshusighrajput007@gmail.com>
2024-12-19 11:09:04 +0530

    Operators

--- operators.java
+++ operators.java
@@ -1,7 +1,7 @@
 public class operators {
     public static void main(String[] args) {
         int a = 2;
-        int b = 6-a;
+        int b = 6-a; //assignment operator and arithmetic operator
         System.out.println(b);
     }
 }
