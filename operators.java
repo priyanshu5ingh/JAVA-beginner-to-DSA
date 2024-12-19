61feba9b79462dec602a2a29ce60a7e031c56960
Priyanshu Singh <priyanshusighrajput007@gmail.com>
2024-12-19 11:13:21 +0530

    Operators

--- operators.java
+++ operators.java
@@ -3,5 +3,7 @@ public class operators {
         int a = 2;
         int b = 6-a; //assignment operator and arithmetic operator
         System.out.println(b);
+        System.out.println(65>4); // Comparision operator
+        System.out.println(64< 6 && 65<6); // Logical operator
     }
 }
